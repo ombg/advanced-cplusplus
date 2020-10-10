f7c34a91a68e237ace271102f821b14195c8763c
Oliver M <olliwaa@gmail.com>
2020-10-09 17:02:10 -0700

    Finished basic exceptions video.

--- BasicExceptions/src/BasicExceptions.cpp
+++ BasicExceptions/src/BasicExceptions.cpp
@@ -11,42 +11,38 @@
 using namespace std;
 
 void mightGoWrong() {
+	
+    bool error1 = false;
+    bool error2 = true;
 
-	bool error1 = false;
-	bool error2 = true;
-
-	if(error1) {
-		throw "Something went wrong.";
-	}
-
-	if(error2) {
-		throw string("Something else went wrong.");
-	}
+    if(error1) {
+        throw "Something went wrong.";
+    }
+    if(error2) {
+        //We don't need to use "new" here. Exception handling cleans up this object.
+        throw string("Somestring else went wrong.");
+    }
 
 }
 
 void usesMightGoWrong() {
-	mightGoWrong();
+    mightGoWrong();
 }
 
-
-
 int main() {
+    try {
+	    usesMightGoWrong();
+    }
+    catch(int e) {
+        cout << "Error code: " << e << endl;
+    }
+    catch(char const * e) {
+        cout << "Error message: " << e << endl;
+    }
+    catch(string& e) {
+        cout << "Error message: " << e << endl;
+    }
+    cout << "Still running" << endl;
 
-	try {
-		usesMightGoWrong();
-	}
-	catch(int e) {
-		cout << "Error code: " << e << endl;
-	}
-	catch(char const * e) {
-		cout << "Error message: " << e << endl;
-	}
-	catch(string &e) {
-		cout << "string error message: " << e << endl;
-	}
-
-	cout << "Still running" << endl;
-
-	return 0;
+    return 0;
 }
